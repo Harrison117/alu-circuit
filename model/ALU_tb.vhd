c97776173454dc41b816221732911720299c4062
KimEzekiel <kldelmundo@up.edu.ph>
2019-02-15 01:23:10 +0800

    done tb for logic gates

--- model/ALU_tb.vhd
+++ model/ALU_tb.vhd
@@ -25,6 +25,63 @@ architecture test of ALU_tb is
   	process
 			variable test_input : std_logic_vector(0 to 6);
 			begin
+
+      report "testing and";
+      test_input:="0000000";
+      inputs <= test_input;
+      for count in 0 to 15 loop
+        inputs <= test_input;
+        wait for 10 ns;
+        assert outputs=(inputs(3 to 4) and inputs(5 to 6) )
+        report  integer'image(to_integer(unsigned(inputs(3 to 4)))) &" + "
+                & integer'image(to_integer(unsigned(inputs(5 to 6)))) & " != "
+                & integer'image(to_integer(unsigned(outputs)));
+        test_input := std_logic_vector(unsigned(test_input)  +1);
+      end loop;
+
+      report "testing or";
+      test_input:="0010000";
+      inputs <= test_input;
+      for count in 0 to 15 loop
+        inputs <= test_input;
+        wait for 10 ns;
+        assert outputs=(inputs(3 to 4) or inputs(5 to 6) )
+        report  integer'image(to_integer(unsigned(inputs(3 to 4)))) &" + "
+                & integer'image(to_integer(unsigned(inputs(5 to 6)))) & " != "
+                & integer'image(to_integer(unsigned(outputs)));
+        test_input := std_logic_vector(unsigned(test_input)  +1);
+      end loop;
+
+      report "testing xor";
+      test_input:="0100000";
+      inputs <= test_input;
+      for count in 0 to 15 loop
+        inputs <= test_input;
+        wait for 10 ns;
+        assert outputs=(inputs(3 to 4) xor inputs(5 to 6) )
+        report  integer'image(to_integer(unsigned(inputs(3 to 4)))) &" + "
+                & integer'image(to_integer(unsigned(inputs(5 to 6)))) & " != "
+                & integer'image(to_integer(unsigned(outputs)));
+        test_input := std_logic_vector(unsigned(test_input)  +1);
+      end loop;
+
+      report "testing not";
+      test_input:="0110000";
+      inputs <= test_input;
+      for count in 0 to 15 loop
+        inputs <= test_input;
+        wait for 10 ns;
+        assert outputs=(not (inputs(3 to 4)))
+        report "not " &
+        std_logic'image(not outputs(0)) & std_logic'image(not outputs(1)) &
+        " != " & std_logic'image(not inputs(3)) & std_logic'image(not inputs(4));
+        -- integer'image(to_integer(unsigned(inputs(3 to 4)))) &" + "
+                -- & integer'image(to_integer(unsigned(inputs(5 to 6)))) & " != "
+                -- & integer'image(to_integer(unsigned(outputs)));
+        test_input := std_logic_vector(unsigned(test_input)  +1);
+      end loop;
+
+
       report "testing addition";
 			test_input := "1000000"; -- binary number 0 (2 bits)
       inputs <= test_input;
