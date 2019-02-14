3c5512111887e7f7284f3f26d8d291bffeb51986
Harrison117 <hsumali@up.edu.ph>
2019-02-15 00:38:22 +0800

    fixed increment implementation

--- model/increment.vhd
+++ model/increment.vhd
@@ -2,7 +2,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-
 entity increment is
 	port(
 		a,b: in std_logic_vector(0 to 1);
@@ -12,14 +11,15 @@ entity increment is
 end increment;
 
 architecture behav of increment is
-	component twos_c_2 is
+	component parallel_adder_2_2 is
 		port(
-			a: in std_logic_vector(0 to 1);
-			o: out std_logic_vector(0 to 1)
+			a, b: in std_logic_vector(0 to 1);
+			o: out std_logic_vector(0 to 1):="00";
+			c_out_2: out std_logic:='0'
 			);
 	end component;
 
 begin
-	inc: twos_c_2 port map(a,o);
+	inc: parallel_adder_2_2 port map(a,"01",o);
 	c_out <= a(0) and a(1);
 end architecture;
